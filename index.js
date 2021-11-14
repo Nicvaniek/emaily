d3b6a4c109d64aa983300ca673fbc3f15157c438
Nicolai van Niekerk <nicvaniek@gmail.com>
2021-11-14 11:32:29 +0200

    finish auth flow

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const express = require('express');
+const authRoutes = require('./routes/auth-routes');
+const keys = require('./config/keys');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+require('./models/user');
+require('./services/passport-service')
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'ccccc' });
-});
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session())
+
+authRoutes(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
